bf57c37438c15c33f0c9213cd9492e569e4db388
stm.sireniti@gmail.com <stm.sireniti@gmail.com>
2026-06-18 17:46:16 +0300

    Добавлен runImage()

--- main.cpp
+++ main.cpp
@@ -23,22 +23,15 @@ std::string recognizePlate(cv::Mat plate,
     const std::string alphabet ="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     cv::Mat gray;
 
-    cv::cvtColor(plate,
-                 gray,
-                 cv::COLOR_BGR2GRAY);
+    cv::cvtColor(plate,gray,cv::COLOR_BGR2GRAY);
 
-    cv::resize(gray,
-               gray,
-               cv::Size(128, 32));
+    cv::resize(gray,gray,cv::Size(128, 32));
 
-    gray.convertTo(gray,
-                   CV_32F,
-                   1.0 / 255.0);
+    gray.convertTo(gray,CV_32F,1.0 / 255.0);
 
     gray = (gray - 0.5f) / 0.5f;
 
-    cv::Mat blob =
-        cv::dnn::blobFromImage(gray);
+    cv::Mat blob = cv::dnn::blobFromImage(gray);
 
     ocrNet.setInput(blob);
 
@@ -95,20 +88,13 @@ std::vector<cv::Rect> detectPlates(cv::Mat& frame,cv::dnn::Net& net,float confTh
 
     cv::Mat blob;
 
-    cv::dnn::blobFromImage(frame,
-                           blob,
-                           1.0 / 255.0,
-                           cv::Size(640,640),
-                           cv::Scalar(),
-                           true,
-                           false);
+    cv::dnn::blobFromImage(frame, blob,1.0 / 255.0,cv::Size(640,640),cv::Scalar(),true,false);
 
     net.setInput(blob);
 
     std::vector<cv::Mat> outputs;
 
-    net.forward(outputs,
-                net.getUnconnectedOutLayersNames());
+    net.forward(outputs,net.getUnconnectedOutLayersNames());
 
     cv::Mat output = outputs[0];
     /*
@@ -123,10 +109,7 @@ std::vector<cv::Rect> detectPlates(cv::Mat& frame,cv::dnn::Net& net,float confTh
     float xFactor = frame.cols / 640.0f;
     float yFactor = frame.rows / 640.0f;
 
-    cv::Mat out(output.size[1],
-                output.size[2],
-                CV_32F,
-                output.ptr<float>());
+    cv::Mat out(output.size[1], output.size[2], CV_32F,output.ptr<float>());
 
     cv::transpose(out, out);
 
@@ -149,30 +132,20 @@ std::vector<cv::Rect> detectPlates(cv::Mat& frame,cv::dnn::Net& net,float confTh
         int width  = int(w * xFactor);
         int height = int(h * yFactor);
 
-        boxes.push_back(
-            cv::Rect(left, top, width, height)
-            );
+        boxes.push_back(cv::Rect(left, top, width, height));
 
         confidences.push_back(confidence);
     }
 
     std::vector<int> indices;
 
-    cv::dnn::NMSBoxes(
-        boxes,
-        confidences,
-        confThreshold,
-        nmsThreshold,
-        indices
-        );
+    cv::dnn::NMSBoxes(boxes,confidences,confThreshold,nmsThreshold,indices);
 
     std::vector<cv::Rect> result;
 
     for(size_t i = 0; i < indices.size(); i++)
     {
-        result.push_back(
-            boxes[indices[i]]
-            );
+        result.push_back(boxes[indices[i]]);
     }
 
     return result;
@@ -197,63 +170,34 @@ void runOCRFolder(const QString& folderPath,
     QDir dir(folderPath);
 
     QStringList filters;
-    filters << "*.jpg"
-            << "*.jpeg"
-            << "*.png"
-            << "*.bmp";
+    filters << "*.jpg"<< "*.jpeg" << "*.png"<< "*.bmp";
 
-    QFileInfoList files =
-        dir.entryInfoList(filters,
-                          QDir::Files);
+    QFileInfoList files =dir.entryInfoList(filters, QDir::Files);
 
     for(const QFileInfo& fileInfo : files)
     {
-        QString fileName =
-            fileInfo.fileName();
+        QString fileName =fileInfo.fileName();
 
-        QString fullPath =
-            fileInfo.absoluteFilePath();
+        QString fullPath =fileInfo.absoluteFilePath();
 
-        cv::Mat plate =
-            cv::imread(
-                fullPath.toStdString()
-                );
+        cv::Mat plate =cv::imread(fullPath.toStdString());
 
         if(plate.empty())
         {
-            std::cout
-                << "Image load error: "
-                << fullPath.toStdString()
-                << std::endl;
+            std::cout<< "Image load error: "<< fullPath.toStdString()<< std::endl;
 
             continue;
         }
 
-        auto t0 =
-            std::chrono::high_resolution_clock::now();
+        auto t0 =std::chrono::high_resolution_clock::now();
 
-        std::string text =
-            recognizePlate(
-                plate,
-                ocrNet
-                );
+        std::string text =recognizePlate(plate,ocrNet);
 
-        auto t1 =
-            std::chrono::high_resolution_clock::now();
+        auto t1 =std::chrono::high_resolution_clock::now();
 
-        double ocrMs =
-            std::chrono::duration<double,std::milli>(
-                t1 - t0
-                ).count();
+        double ocrMs =std::chrono::duration<double,std::milli>(t1 - t0).count();
 
-        std::cout
-            << fileName.toStdString()
-            << " -> "
-            << text
-            << " [OCR="
-            << ocrMs
-            << " ms]"
-            << std::endl;
+        std::cout<< fileName.toStdString() << " -> "<< text<< " [OCR="<< ocrMs<< " ms]"<< std::endl;
     }
 }
 
@@ -323,126 +267,87 @@ void runFolder(const QString& folderPath, cv::dnn::Net& yoloNet, cv::dnn::Net& o
     }
 }
 
-/*
-void runVideo(const std::string& videoPath,
-              cv::dnn::Net& yoloNet,
-              cv::dnn::Net& ocrNet)
+void runImage(const QString& imagePath, cv::dnn::Net& yoloNet, cv::dnn::Net& ocrNet)
 {
-    cv::VideoCapture cap(videoPath);
+    QFileInfo fileInfo(imagePath);
 
+    QString fileName = fileInfo.fileName();
+    QString fullPath = fileInfo.absoluteFilePath();
 
-        if(!cap.isOpened())
+    cv::Mat frame = cv::imread(fullPath.toStdString());
+
+    if(frame.empty())
     {
-        std::cout << "Video open error: "
-                  << videoPath
-                  << std::endl;
+        std::cout << "Image load error: " << fullPath.toStdString() << std::endl;
         return;
     }
 
-    cv::Mat frame;
+    auto totalStart = std::chrono::high_resolution_clock::now();
 
-    int frameCounter = 0;
+    auto yoloStart = std::chrono::high_resolution_clock::now();
+    auto boxes = detectPlates(frame, yoloNet);
+    auto yoloEnd = std::chrono::high_resolution_clock::now();
 
-    while(cap.read(frame))
+    std::cout << fileName.toStdString() << " boxes=" << boxes.size() << " -> ";
+
+    if(boxes.empty())
     {
-        frameCounter++;
-
-        // можно пропускать кадры
-        if(frameCounter % 20 != 0)
-            continue;
-
-        auto totalStart =
-            std::chrono::high_resolution_clock::now();
-
-        auto boxes =
-            detectPlates(frame, yoloNet);
-
-        for(auto& box : boxes)
-        {
-            cv::Rect safeBox =
-                box &
-                cv::Rect(
-                    0,
-                    0,
-                    frame.cols,
-                    frame.rows
-                    );
-
-            if(safeBox.width <= 0 ||
-                safeBox.height <= 0)
-            {
-                continue;
-            }
-
-            cv::Mat plate =
-                frame(safeBox).clone();
-
-            std::string text =
-                recognizePlate(
-                    plate,
-                    ocrNet
-                    );
-
-            cv::rectangle(
-                frame,
-                safeBox,
-                cv::Scalar(0,255,0),
-                2
-                );
-
-            cv::putText(
-                frame,
-                text,
-                cv::Point(
-                    safeBox.x,
-                    safeBox.y - 10
-                    ),
-                cv::FONT_HERSHEY_SIMPLEX,
-                0.8,
-                cv::Scalar(0,255,0),
-                2
-                );
-        }
-
-        auto totalEnd =
-            std::chrono::high_resolution_clock::now();
-
-        double totalMs =
-            std::chrono::duration<double,std::milli>(
-                totalEnd - totalStart
-                ).count();
-
-        double fps =
-            1000.0 / totalMs;
-
-        cv::putText(
-            frame,
-            cv::format("FPS %.2f", fps),
-            cv::Point(20,40),
-            cv::FONT_HERSHEY_SIMPLEX,
-            1.0,
-            cv::Scalar(0,255,255),
-            2
-            );
+        std::cout << "NO_PLATE" << std::endl;
 
         cv::imshow("ANPR", frame);
+        cv::waitKey(0);
+        cv::destroyAllWindows();
 
-        int key = cv::waitKey(1);
-
-        if(key == 27)
-            break;
+        return;
     }
 
-    cap.release();
+    double ocrMs = 0.0;
+
+    for(size_t i = 0; i < boxes.size(); i++)
+    {
+        cv::Rect safeBox = boxes[i] & cv::Rect(0, 0, frame.cols, frame.rows);
+
+        if(safeBox.width <= 0 || safeBox.height <= 0)
+            continue;
+
+        cv::Mat plate = frame(safeBox).clone();
+
+        auto ocrStart = std::chrono::high_resolution_clock::now();
+
+        std::string text = recognizePlate(plate, ocrNet);
+
+        auto ocrEnd = std::chrono::high_resolution_clock::now();
+
+        ocrMs += std::chrono::duration<double, std::milli>(ocrEnd - ocrStart).count();
+
+        std::cout << text;
+
+        if(i + 1 < boxes.size())
+            std::cout << " ";
+
+        cv::rectangle(frame, safeBox, cv::Scalar(255, 255, 0), 1);
+
+        cv::putText(frame,text,cv::Point(safeBox.x, safeBox.y - 10),cv::FONT_HERSHEY_SIMPLEX,0.5,cv::Scalar(255, 255, 0),1);
+    }
+
+    auto totalEnd = std::chrono::high_resolution_clock::now();
+
+    double yoloMs = std::chrono::duration<double, std::milli>(yoloEnd - yoloStart).count();
+    double totalMs = std::chrono::duration<double, std::milli>(totalEnd - totalStart).count();
+    double fps = 1000.0 / totalMs;
+
+    std::cout << " [YOLO=" << yoloMs<< " ms OCR=" << ocrMs<< " ms TOTAL=" << totalMs<< " ms FPS=" << fps<< "]" << std::endl;
+
+    cv::putText( frame,cv::format("FPS %.2f", fps),cv::Point(20, 40),cv::FONT_HERSHEY_SIMPLEX,0.8,cv::Scalar(255, 255, 0),1);
+
+    cv::imshow("ANPR", frame);
+
+    cv::waitKey(0);
+
     cv::destroyAllWindows();
-
-
 }
-*/
 
-void runVideo(const std::string& videoPath,
-              cv::dnn::Net& yoloNet,
-              cv::dnn::Net& ocrNet)
+void runVideo(const std::string& videoPath,cv::dnn::Net& yoloNet,cv::dnn::Net& ocrNet)
 {
     cv::VideoCapture cap(videoPath);
     // cv::VideoCapture cap;
@@ -450,9 +355,7 @@ void runVideo(const std::string& videoPath,
 
     if(!cap.isOpened())
     {
-        std::cout << "Video open error: "
-                  << videoPath
-                  << std::endl;
+        std::cout << "Video open error: "<< videoPath<< std::endl;
         return;
     }
     /*
@@ -486,7 +389,7 @@ void runVideo(const std::string& videoPath,
 
 
     std::map<std::string,int> plateCounter;
-std::filesystem::create_directories("captures");
+    std::filesystem::create_directories("captures");
     while(cap.read(frame))
     {
         frameCounter++;
@@ -495,66 +398,38 @@ std::filesystem::create_directories("captures");
         if(frameCounter % 3 != 0)
             continue;
 
-        auto totalStart =
-            std::chrono::high_resolution_clock::now();
+        auto totalStart =std::chrono::high_resolution_clock::now();
 
-        auto boxes =
-            detectPlates(frame, yoloNet);
+        auto boxes =detectPlates(frame, yoloNet);
 
         for(auto& box : boxes)
         {
-            cv::Rect safeBox =
-                box &
-                cv::Rect(
-                    0,
-                    0,
-                    frame.cols,
-                    frame.rows
-                    );
+            cv::Rect safeBox = box &cv::Rect(0,0,frame.cols,frame.rows);
 
-            if(safeBox.width <= 0 ||
-                safeBox.height <= 0)
+            if(safeBox.width <= 0 ||safeBox.height <= 0)
             {
                 continue;
             }
 
-            cv::Mat plate =
-                frame(safeBox).clone();
+            cv::Mat plate =frame(safeBox).clone();
 
-            std::string text =
-                recognizePlate(
-                    plate,
-                    ocrNet
-                    );
+            std::string text =recognizePlate( plate,ocrNet);
 
             //
             if(text.length() == 8)
             {
                 plateCounter[text]++;
-                std::cout
-                    << text
-                    << " count="
-                    << plateCounter[text]
-                    << std::endl;
+                std::cout<< text << " count="<< plateCounter[text]<< std::endl;
 
                 if(plateCounter[text] >= 3)
                 {
-                    if(savedPlates.find(text) ==
-                        savedPlates.end())
+                    if(savedPlates.find(text) == savedPlates.end())
                     {
                         savedPlates.insert(text);
 
-                        cv::imwrite(
-                            "captures/" +
-                                text +
-                                ".jpg",
-                            plate
-                            );
+                        cv::imwrite("captures/" +text +".jpg",plate);
 
-                        std::cout
-                            << "Saved plate: "
-                            << text
-                            << std::endl;
+                        std::cout<< "Saved plate: "<< text<< std::endl;
                     }
                 }
             }
@@ -585,47 +460,18 @@ std::filesystem::create_directories("captures");
             }
             */
             //
-            cv::rectangle(
-                frame,
-                safeBox,
-                cv::Scalar(0,255,0),
-                2
-                );
+            cv::rectangle(frame,safeBox,cv::Scalar(0,255,0),2);
 
-            cv::putText(
-                frame,
-                text,
-                cv::Point(
-                    safeBox.x,
-                    safeBox.y - 10
-                    ),
-                cv::FONT_HERSHEY_SIMPLEX,
-                0.8,
-                cv::Scalar(0,255,0),
-                2
-                );
+            cv::putText(frame,text,cv::Point(safeBox.x,safeBox.y - 10 ),cv::FONT_HERSHEY_SIMPLEX,0.8,cv::Scalar(0,255,0),2);
         }
 
-        auto totalEnd =
-            std::chrono::high_resolution_clock::now();
+        auto totalEnd =std::chrono::high_resolution_clock::now();
 
-        double totalMs =
-            std::chrono::duration<double,std::milli>(
-                totalEnd - totalStart
-                ).count();
+        double totalMs =std::chrono::duration<double,std::milli>(totalEnd - totalStart).count();
 
-        double fps =
-            1000.0 / totalMs;
+        double fps =1000.0 / totalMs;
 
-        cv::putText(
-            frame,
-            cv::format("FPS %.2f", fps),
-            cv::Point(20,40),
-            cv::FONT_HERSHEY_SIMPLEX,
-            1.0,
-            cv::Scalar(0,255,255),
-            2
-            );
+        cv::putText(frame,cv::format("FPS %.2f", fps),cv::Point(20,40),cv::FONT_HERSHEY_SIMPLEX,1.0,cv::Scalar(0,255,255),2);
 
         //writer.write(frame);
 
@@ -660,14 +506,12 @@ std::filesystem::create_directories("captures");
 int main(int argc, char *argv[])
 {
     //QApplication a(argc, argv);
- QCoreApplication a(argc, argv);
+    QCoreApplication a(argc, argv);
     std::cout << CV_VERSION << std::endl;
 
-    cv::utils::logging::setLogLevel(
-        cv::utils::logging::LOG_LEVEL_SILENT);
+    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_SILENT);
 
-    cv::dnn::Net net =
-        cv::dnn::readNetFromONNX("model/best.onnx");
+    cv::dnn::Net net =cv::dnn::readNetFromONNX("model/best.onnx");
 
     if(net.empty())
     {
@@ -678,11 +522,9 @@ int main(int argc, char *argv[])
     net.setPreferableBackend(
         cv::dnn::DNN_BACKEND_OPENCV);
 
-    net.setPreferableTarget(
-        cv::dnn::DNN_TARGET_CPU);
+    net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
 
-    cv::dnn::Net ocrNet =
-        cv::dnn::readNetFromONNX("model/crnn_ua_113.onnx");
+    cv::dnn::Net ocrNet =cv::dnn::readNetFromONNX("model/crnn_ua_113.onnx");
 
     if(ocrNet.empty())
     {
@@ -690,19 +532,17 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    ocrNet.setPreferableBackend(
-        cv::dnn::DNN_BACKEND_OPENCV);
+    ocrNet.setPreferableBackend(cv::dnn::DNN_BACKEND_OPENCV);
 
-    ocrNet.setPreferableTarget(
-        cv::dnn::DNN_TARGET_CPU);
+    ocrNet.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
 
     std::cout << "OCR loaded" << std::endl;
 
 
-    runFolder("C:/Users/SOTY_PC/Desktop/linux prim/ANPR-X86/img", net,ocrNet);
+   // runFolder("C:/Users/SOTY_PC/Desktop/linux prim/ANPR-X86/img", net,ocrNet);
 
     //runVideo("C:/Users/SOTY_PC/Desktop/linux prim/ANPR-X86/Video/1_stable.mp4",net, ocrNet);
-
+      runImage("img/BI3157CX.jpg", net, ocrNet);
 
 
 
